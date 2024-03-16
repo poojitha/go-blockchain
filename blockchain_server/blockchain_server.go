cf5226ca2fa0bc7bc13f5c60a1a5ab6f7185443f
Poojitha Jayasinghe <indrakeerthi@gmail.com>
2024-03-16 23:17:43 +1100

    block chain server setup

---
+++ blockchain_server/blockchain_server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"go-blockchain/blockchain/block"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+var cache map[string]*block.Blockchain = make(map[string]*block.Blockchain)
+
+type BlockchainServer struct {
+	port uint16
+}
+
+func NewBlockchainServer(port uint16) *BlockchainServer {
+	return &BlockchainServer{port}
+}
+
+func (bcs *BlockchainServer) Port() uint16 {
+	return bcs.port
+}
+
+func HelloWorld(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello World!")
+}
+
+func (bcs *BlockchainServer) Run() {
+	http.HandleFunc("/", HelloWorld)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(bcs.Port())), nil))
+}
---
+++ blockchain_server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+
+func main() {
+	port := flag.Uint("port", 5000, "TCP port number for Blockchian Server")
+	flag.Parse()
+	fmt.Println(*port)
+	app := NewBlockchainServer(uint16(*port))
+	app.Run()
+
+}
